c8d995424fc06479f1c3b75dd41dafaea425dc6b
PoojithaKanagala <kanagalapoojitha98@gmail.com>
2024-10-22 23:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+
+    const digitalTime = (h < 10 ? '0' : '') + h + ':' + (m < 10 ? '0' : '') + m + ':' + (s < 10 ? '0' : '') + s;
+    document.getElementById('digital-time').textContent = digitalTime;
+
+}, 1000);
